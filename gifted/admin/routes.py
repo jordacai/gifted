d417a799a03f990efd44bfcd4a7e27677c6e9896
jcaimano <jordan.caimano@gmail.com>
2019-11-03 22:27:17 -0600

    Replaced various forms with modals.

--- gifted/admin/routes.py
+++ gifted/admin/routes.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from flask import Blueprint, render_template, request, flash, url_for, current_app, session
 from flask_mail import Message
+from werkzeug import security
 from werkzeug.utils import redirect
 
 from gifted import db, mail
@@ -19,7 +20,7 @@ def index():
     return render_template('admin.html', events=events, invites=invites)
 
 
-@admin.route('/admin/events', methods=['GET', 'POST'])
+@admin.route('/admin/events', methods=['POST'])
 def create_event():
     if request.method == 'POST':
         title = request.form.get('title')
@@ -58,6 +59,29 @@ def delete_event(event_id):
     db.session.commit()
     flash(f'Deleted event {event}!', 'success')
     return redirect(url_for('admin.index'))
+
+
+@admin.route('/admin/events/<event_id>/users', methods=['POST'])
+def create_user(event_id):
+    username = request.form.get('username')
+    password = request.form.get('password')
+    password_confirm = request.form.get('passwordConfirm')
+    first_name = request.form.get('firstName')
+    last_name = request.form.get('lastName')
+
+    if password != password_confirm:
+        flash('Passwords must match!', 'error')
+        return redirect(url_for('admin.get_event', event_id=event_id))
+
+    user = User(username=username, password=security.generate_password_hash(password),
+                first_name=first_name, last_name=last_name)
+
+    event = Event.query.get(event_id)
+    event.users.append(user)
+    db.session.add(user)
+    db.session.commit()
+    flash(f'Created user {user}!', 'success')
+    return redirect(url_for('admin.get_event', event_id=event_id))
 
 
 @admin.route('/admin/users/<user_id>/delete', methods=['POST'])
