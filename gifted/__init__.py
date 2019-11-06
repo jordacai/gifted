37c179c2bc910cc94eddab514e2d88bbe4049bd8
jcaimano <jordan.caimano@gmail.com>
2019-11-06 12:53:43 -0600

    Added talisman for auto https redirection.

--- gifted/__init__.py
+++ gifted/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_talisman import Talisman
 
 from config import Config
 from gifted.helpers import validate, login_required
@@ -12,6 +13,15 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 mail = Mail(app)
+csp = {
+ 'default-src': [
+        '\'self\'',
+        '*.bootstrapcdn.com',
+        '*.jquery.com',
+        '*.cloudflare.com'
+    ]
+}
+Talisman(app, content_security_policy=csp)
 
 from gifted import models
 from .admin.routes import admin
