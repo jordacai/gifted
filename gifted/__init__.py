30a64fa5b2f13a85ea6daa93638508ca329c2afa
jcaimano <jordan.caimano@gmail.com>
2019-11-05 20:36:42 -0600

    Cleaned up a bit for Heroku deployment.

--- gifted/__init__.py
+++ gifted/__init__.py
@@ -1,39 +1,33 @@
-import os
-
 from flask import Flask
 from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
+from config import Config
 from gifted.helpers import validate, login_required
 
-db = SQLAlchemy()
-mail = Mail()
-migrate = Migrate()
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+mail = Mail(app)
+
+from gifted import models
+from .admin.routes import admin
+from .main.routes import main
+app.register_blueprint(admin)
+app.register_blueprint(main)
 
 
-def create_app():
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    with app.app_context():
-        app.config.from_pyfile('config.py')
-    db.init_app(app)
-    mail.init_app(app)
-    migrate.init_app(app, db)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    with app.app_context():
-        from .admin.routes import admin
-        from .main.routes import main
-        app.register_blueprint(admin)
-        app.register_blueprint(main)
-        return app
-
-
-if __name__ == '__main__':
-    create_app().run()
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        'db': db,
+        'Transaction': models.Transaction,
+        'Pair': models.Pair,
+        'User': models.User,
+        'Invite': models.Invite,
+        'Event': models.Event,
+        'Item': models.Item
+    }
