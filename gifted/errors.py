88a55e629ce00bd66a17c57a9afeb721085df801
jcaimano <jordan.caimano@gmail.com>
2019-11-06 20:42:01 -0600

    Fixed misnamed error template.

--- gifted/errors.py
+++ gifted/errors.py
@@ -6,4 +6,4 @@ from gifted import app, db
 @app.errorhandler(500)
 def internal_error(error):
     db.session.rollback()
-    return render_template('500.html'), 500
+    return render_template('internal_error.html'), 500
