1edb928e8c87544c8453ad46c2a15b15a59c4ac1
jcaimano <jordan.caimano@gmail.com>
2020-11-20 18:53:21 -0600

    Added ability to fetch images from Amazon links
    > Added a few @template_functions to check if an item url is Amazon,
    and if so then use regex to parse the ASIN to be passed to an img src.
    > Now preventing child progress from being shown on parent's event page.
    Progress rendered would "ruin the surprise", and the tradeoff is that
    parents cannot purchase items off their child's list with this approach.
    > Now displaying item priority on wishlist page.

--- gifted/static/scripts.js
+++ gifted/static/scripts.js
@@ -1,4 +1,11 @@
 function confirmDelete() {
+    if (!confirm("Are you sure? There is a chance a gifter already purchased this, and removing it would affect their" +
+    " purchase history.")) {
+        return false;
+    }
+}
+
+function confirmUnclaim() {
     if (!confirm("Are you sure? Since you already claimed this item, it may not be picked up by another gifter.")) {
         return false;
     }
